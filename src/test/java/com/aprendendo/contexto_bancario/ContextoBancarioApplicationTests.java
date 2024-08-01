22c073b8e5073a8712ab0a7286a37e1b2256bcd8
Angelo Carvalho <angelosuporteti@gmail.com>
2024-08-01 13:54:35 -0300

    Projeto iniciado

---
+++ src/test/java/com/aprendendo/contexto_bancario/ContextoBancarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aprendendo.contexto_bancario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContextoBancarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
