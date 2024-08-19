41215eda1456575d247dcaa1b232a7545ea5acc7
Angelo Carvalho <angelosuporteti@gmail.com>
2024-08-19 16:53:23 -0300

    Provisionando BD

--- src/main/java/com/aprendendo/contexto_bancario/Application.java
+++ src/main/java/com/aprendendo/contexto_bancario/Application.java
@@ -3,6 +3,7 @@ package com.aprendendo.contexto_bancario;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class Application {
 
@@ -10,3 +11,8 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 }
+
+/**
+ * Sobre arquivo .properties ha uma pergunta respondida em:
+ * https://pt.stackoverflow.com/questions/245052/2-arquivos-properties-no-spring-boot
+ */
